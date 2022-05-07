5250cba7ecc82f49d2595c20340c2a37b67a307e
Simon Frankau <sgf@arbitrary.name>
2022-05-07 23:01:53 +0100

    Move extract_cells to subdir, to make way for other tools

--- tools/extract_cells/src/main.rs
+++ tools/extract_cells/src/main.rs
@@ -56,8 +56,8 @@ fn draw_cell(img: &mut Image, x: usize, y:usize, data: &[u8]) {
     // Inefficient, but can't iterate an array, or return an iterator over it.
     let mut pixel_iter = data.iter().flat_map(|p| vec![p >> 4, p & 0xf]);
 
-    for x_off in 0..CELL_SIZE {
-        for y_off in 0..CELL_SIZE {
+    for y_off in 0..CELL_SIZE {
+        for x_off in 0..CELL_SIZE {
             let raw_pixel = pixel_iter.next().unwrap();
             let pixel = raw_pixel * 16; // TODO: Palette lookup.
             img.set_pixel(x + x_off, y + y_off, pixel);
@@ -89,9 +89,11 @@ fn build_image(data: &[u8], w: usize, h: usize) -> Image {
 }
 
 fn main() {
-    let data = fs::read("../speedball2-usa.bin").unwrap();
+    let data = fs::read("../../speedball2-usa.bin").unwrap();
+    let total_cells = 128 * 128;
 
-    let img = build_image(&data, 128, 128);
-
-    img.save(Path::new("cells.png"));
+    for w in 144..=144 {
+        let img = build_image(&data, w, total_cells / w);
+        img.save(Path::new(format!("cells-{:02}.png", w).as_str()));
+    }
 }
