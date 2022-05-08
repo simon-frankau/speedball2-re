3330552329511e15dcd2d72eefefef39ae894eca
Simon Frankau <sgf@arbitrary.name>
2022-05-08 10:30:25 +0100

    Make an image of the potential palettes, help see the wood for the trees

--- tools/find_palettes/src/main.rs
+++ tools/find_palettes/src/main.rs
@@ -6,6 +6,8 @@
 
 use std::fs;
 
+use image::RgbImage;
+
 // 16 colour palette.
 const PALETTE_SIZE: usize = 16;
 // 2 bytes per entry.
@@ -22,7 +24,7 @@ fn looks_like_colour(data: &[u8]) -> bool {
 // Looks like a palette if we have 16 consecutive colours.
 fn looks_like_palette(data: &[u8]) -> bool {
     for i in 0..16 {
-        if !looks_like_colour(&data[2 * i ..]) {
+        if !looks_like_colour(&data[2 * i..]) {
             return false;
         }
     }
@@ -31,8 +33,44 @@ fn looks_like_palette(data: &[u8]) -> bool {
     data[..16].iter().any(|x| *x != 0)
 }
 
+fn extract_colour(data: &[u8]) -> Vec<u8> {
+    // 3-bit RGB values.
+    let r = (data[1] >> 1) & 7;
+    let g = (data[1] >> 5) & 7;
+    let b = (data[0] >> 1) & 7;
+
+    vec![r << 5, g << 5, b << 5]
+}
+
+fn palette_to_image_row(palette: &[u8]) -> Vec<u8> {
+    palette
+        .chunks_exact(2)
+        .flat_map(|v| extract_colour(v))
+        .collect()
+}
+
+fn palettes_to_image(palettes: &[&[u8]]) -> RgbImage {
+    let width = palettes.iter().map(|v| v.len()).max().unwrap();
+    let height = palettes.len();
+
+    let image_data = palettes
+        .iter()
+        .flat_map(|v| {
+            let mut row = palette_to_image_row(v);
+            row.resize(width * 3, 255);
+            row
+        })
+        .collect();
+
+    RgbImage::from_vec(width as u32, height as u32, image_data).unwrap()
+}
+
 fn main() {
     let data = fs::read("../../speedball2-usa.bin").unwrap();
+
+    // Find all the potential palettes, printing their locations and
+    // stashing the palettes.
+    let mut palettes: Vec<&[u8]> = Vec::new();
 
     // Palettes tend to be odd and even, so this logic is messy...
     let mut last_maybe_palette = false;
@@ -45,8 +83,13 @@ fn main() {
             start = idx;
         } else if !maybe_palette && !last_maybe_palette && last_maybe_palette2 {
             println!("{:06x} ({})", idx, idx - start);
+            palettes.push(&data[start..idx - 2 + PALETTE_LEN]);
         }
         last_maybe_palette2 = last_maybe_palette;
         last_maybe_palette = maybe_palette;
     }
+
+    // And now, make a lovely little picture of the possible palettes.
+    let image = palettes_to_image(&palettes);
+    image.save("palettes.png").unwrap();
 }
