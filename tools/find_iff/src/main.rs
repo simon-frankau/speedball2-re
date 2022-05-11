92be3b29f4583f3e2fee459af82fcae7bc5d0d7c
Simon Frankau <sgf@arbitrary.name>
2022-05-11 11:29:21 +0100

    Make find_iff selective enough to pull out the real IFF blocks

--- tools/find_iff/src/main.rs
+++ tools/find_iff/src/main.rs
@@ -4,13 +4,22 @@
 // Find the addresses that may be the start of an IFF block.
 //
 
+use std::collections::HashSet;
 use std::fs;
+use std::str;
 
 // From previous reversing, we know everything after this address is
 // graphics.
 const SEARCH_END: usize = 0x02fd0a;
 
-fn looks_like_iff_header(data: &[u8], addr: usize) -> bool {
+struct IFF {
+    tag: String,
+    start: usize,
+    end: usize,
+    len: usize,
+}
+
+fn try_find_iff_header(data: &[u8], addr: usize) -> Option<IFF> {
     let candidate = &data[addr..];
 
     fn is_printable_ascii(c: &u8) -> bool {
@@ -19,7 +28,7 @@ fn looks_like_iff_header(data: &[u8], addr: usize) -> bool {
 
     // Tag must be printable ASCII.
     if !candidate.iter().take(4).all(is_printable_ascii) {
-        return false;
+        return None;
     }
 
     // Chunk must not be empty and must fit in ROM.
@@ -29,21 +38,49 @@ fn looks_like_iff_header(data: &[u8], addr: usize) -> bool {
     let end = addr + len + 8;
 
     if len == 0 || end > SEARCH_END {
-        return false;
+        return None;
     }
 
-    println!("Candidate at {:06x}, length {:06x}, finishes {:06x}",
-        addr, len, end);
-
-    true
+    Some(IFF {
+        tag: str::from_utf8(&candidate[..4]).unwrap().to_string(),
+        start: addr,
+        end,
+        len,
+    })
 }
 
 fn main() {
     let data = fs::read("../../speedball2-usa.bin").unwrap();
 
+    // Our algorithm is to mark as high confidence any IFF block
+    // that's part of a chain - i.e. the end of one block matches the
+    // start of another.
+
+    // Could have done more functional style. Didn't feel like it. :)
+    let mut candidates = Vec::new();
     for addr in 0..SEARCH_END {
-        if looks_like_iff_header(&data, addr) {
-            // Nothing for now.
+        if let Some(iff) = try_find_iff_header(&data, addr) {
+            candidates.push(iff);
+        }
+    }
+
+    // Candidates are ordered, so this isn't strictly necessary...
+    let starts = candidates.iter().map(|c| c.start).collect::<HashSet<_>>();
+
+    let mut confident = HashSet::new();
+    for iff in candidates.iter() {
+        if starts.contains(&iff.end) {
+            confident.insert(iff.start);
+            confident.insert(iff.end);
+        }
+    }
+
+    // Only print out the tags we're confident in. Eyeballing the rest
+    // suggests they're all junk!
+    for iff in candidates.iter() {
+        if confident.contains(&iff.start) {
+            println!("Address: {:06x}, Tag: {}, Length: {:06x}, End: {:06x}",
+                iff.start, iff.tag, iff.len, iff.end);
         }
     }
 }
