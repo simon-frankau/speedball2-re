ec021f5918a7c0c15949d5fd78b1cbde1b12ca2a
Simon Frankau <sgf@arbitrary.name>
2022-05-15 16:28:08 +0100

    Reversed the z80 main.asm, more or less.

--- z80/README.md
+++ z80/README.md
@@ -4,7 +4,8 @@ The z80 code from the ROM.
 
  * `reset` Routine extracted from 0x0002c4. Just clears everything and
    goes into an infinite loop.
- * `main` Routine extracted from 0x02e620. TODO: To be decoded.
+ * `main` Routine extracted from 0x02e620. When triggered, reads data
+   from the 68K RAM into the FM1 (channel 1-3) data register.
 
 For each piece of code, I've included the hex dump, binary and
 disassembly.
--- z80/main.asm
+++ z80/main.asm
@@ -1,95 +1,132 @@
 #target ROM
 #code HOH, 0, $FFFF
 
-L0000:	JP	Entry
+	;; Bank select register
+BankReg:	EQU	$6000
+	;; Banked data location
+BankBase:	EQU	$8000
+	;; Data register for FM chip
+FM1Data:	EQU	$4001
 
-	;; Stack.
-L0003:	DEFB $00,$00,$00,$00,$00,$00
-L0009:	DEFB $00
-L000A:	DEFB $00
-	DEFB $00,$00,$00,$00,$00,$00,$00,$00
-	DEFB $00,$00,$00,$00,$00,$00
-        ;; Stack grows down from here.
-L0019:	LD	BC,$6000
-	LD	DE,($0004)
-	LD	A,E
-	LD	E,$01
-	RLCA
-	AND	E
-	LD	(BC),A
-	LD	A,D
-	AND	E
-	LD	(BC),A
-	RRC	D
-	LD	A,D
-	AND	E
-	LD	(BC),A
-	RRC	D
-	LD	A,D
-	AND	E
-	LD	(BC),A
-	RRC	D
-	LD	A,D
-	AND	E
-	LD	(BC),A
-	RRC	D
-	LD	A,D
-	AND	E
-	LD	(BC),A
-	RRC	D
-	LD	A,D
-	AND	E
-	LD	(BC),A
-	RRC	D
-	LD	A,D
-	AND	E
-	LD	(BC),A
-	RRC	D
-	LD	A,D
-	AND	E
-	LD	(BC),A
-	LD	DE,$8000
-	RET
+Start:		JP	Entry
 
-Entry:	DI
-	LD	SP,$0019
-	LD	A,$42
-	LD	(L0009),A
+	;; Effectively the lower byte of the addres,
+	;; but is indexed in z80 space, not 68k space.
+Source:		DEFB	$00
+	;; Mid and upper byte of 68k address.
+BankNum:	DEFW	$0000
+	;; Number of bytes to write.
+ByteCount:	DEFW	$0000
+	;; Signal to do next step. Non-zero triggers it,
+	;; Top bit set breaks out of current write cycle.
+SignalIn:	DEFB	$00
+	;; Signal out about whether we're still processing.
+SignalOut:	DEFB	$00
+	;; Stack space
+StackBase:	DEFB	$00,$00,$00,$00,$00,$00,$00,$00
+		DEFB	$00,$00,$00,$00,$00,$00,$00
+	;; Stack grows down from here.
+StackTop:
 
-L1:	LD	A,($0008)
-	OR	A
-L2:	JP	Z,L1
-	CALL	$0019
-	LD	A,$01
-	LD	($0008),A
-	LD	DE,($0003)
-	SET	7,D
-	LD	HL,($0006)
-	LD	A,$99
-L3:	LD	($0009),A
+	;; Select bank by writing A15-A23 serially into LSB of BankReg.
+SelectBank:	LD	BC,BankReg
+		LD	DE,(BankNum)
+	;; Write top bit of E.
+		LD	A,E
+		LD	E,$01
+		RLCA
+		AND	E
+		LD	(BC),A
+	;; Bottom bit of D.
+		LD	A,D
+		AND	E
+		LD	(BC),A
+	;; Next bit...
+		RRC	D
+		LD	A,D
+		AND	E
+		LD	(BC),A
+		RRC	D
+		LD	A,D
+		AND	E
+		LD	(BC),A
+		RRC	D
+		LD	A,D
+		AND	E
+		LD	(BC),A
+		RRC	D
+		LD	A,D
+		AND	E
+		LD	(BC),A
+		RRC	D
+		LD	A,D
+		AND	E
+		LD	(BC),A
+		RRC	D
+		LD	A,D
+		AND	E
+		LD	(BC),A
+		RRC	D
+		LD	A,D
+		AND	E
+		LD	(BC),A
+	;; Until all 9 bits have been written.
+	;; Point DE at the bank area and return.
+		LD	DE,BankBase
+		RET
 
-	;; Delay loop
-	LD	B,$10
-Delay:	DJNZ	Delay
+Entry:		DI
+		LD	SP,StackTop
+		LD	A,$42
+		LD	(SignalOut),A
 
-	LD	A,($0008)
-	OR	A
-	JP	Z,L3
-	JP	M,L2
-	LD	A,H
-	OR	L
-	JP	Z,L3
-	LD	A,(DE)
-	ADD	A,$80
-	LD	($4001),A
-	INC	DE
-	DEC	HL
-	LD	A,D
-	OR	E
-	JP	NZ,L3
-	LD	IX,($0004)
-	LD	BC,$0080
-	ADD	IX,BC
-	LD	($0004),IX
-	CALL	$0019
-	JP	L3
+L1:		LD	A,(SignalIn)
+		OR	A
+L2:		JP	Z,L1
+		CALL	SelectBan
+	;; Claer top bit, so we don't immediately exit the loop?
+		LD	A,$01
+		LD	(SignalIn),A
+	;; Z80 memory address to start at is 0x8000 | Source,
+	;; where Source is the low-order byte at address 3.
+	;; Slightly tricky code creating a 3-byte 68K addr.
+		LD	DE,(Source)
+		SET	7,D
+		LD	HL,(ByteCount)
+		LD	A,$99
+
+	;; Write bytes from memory to the FM data register.
+	;; HL counts bytes remaining, DE the source address.
+FMWriteLoop:	LD	(SignalOut),A
+
+		;; Delay loop
+		LD	B,$10
+Delay:		DJNZ	Delay
+
+		LD	A,(SignalIn)
+		OR	A
+		JP	Z,FMWriteLoop
+		JP	M,L2
+	;; If HL is zero, nothing to do, loop.
+		LD	A,H
+		OR	L
+		JP	Z,FMWriteLoop
+	;; *FM1Data = *DE++ + 0x80, decrement HL loop counter.
+		LD	A,(DE)
+		ADD	A,$80
+		LD	(FM1Data),A
+		INC	DE
+		DEC	HL
+	;; If DE != 0, loop..
+		LD	A,D
+		OR	E
+		JP	NZ,FMWriteLoop
+	;; DE is zero, we're off the end of the bank, so map
+	;; in the next bank (32k/0x8000 higher)..
+		LD	IX,(BankNum)
+		LD	BC,$0080
+		ADD	IX,BC
+		LD	(BankNum),IX
+		CALL	SelectBank
+	;; And loop.
+		JP	FMWriteLoop
